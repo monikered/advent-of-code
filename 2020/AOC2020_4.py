247f06125b5a2b35e821304141ef809bd54820d6
monikered <monica.remmers@gmail.com>
2021-01-27 22:07:04 +0000

    Added input files and cleaned up day 4 code

--- 2020/AOC2020_4.py
+++ 2020/AOC2020_4.py
@@ -20,31 +20,15 @@ def validator(passport_list):
         else:
             pass
     return counter
-    
-
-test_input = '''
-ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
-
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
-
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
-
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in
-''' 
 
 def test_validator():
     assert validator(test_input) == 2, 'Should be 2 valid passports'
 
 if __name__ == '__main__':
     if run_test:
-        test_validator()
-        print('Test passed')
+        with open('./inputs/day4_test.txt', 'r') as test_input:
+            test_validator()
+            print('Test passed')
     else:
         with open('./inputs/day4.txt', 'r') as f:
             my_password_list = f.read()
