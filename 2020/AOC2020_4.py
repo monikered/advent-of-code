0a2c9900e0fcf0d4985f5f7c96ec2c1efcb5977d
monikered <monica.remmers@gmail.com>
2021-01-29 13:14:41 +0000

    Added part two and refactored

--- 2020/AOC2020_4.py
+++ 2020/AOC2020_4.py
@@ -9,27 +9,42 @@ import re
 
 run_test = False
 
-def validator(passport_list):
-    passports = passport_list.split('\n\n')
-    counter = 0
+def part_one_validator(passports):
+    ValidPassports = 0
     field_list = ['byr:','iyr:','eyr:','hgt:','hcl:','ecl:','pid:']
     fields = re.compile('|'.join(field_list))
     for passport in passports:
         if len(fields.findall(passport)) == 7:
-                counter += 1
+            ValidPassports += 1
         else:
             pass
-    return counter
+    return ValidPassports
+
+def part_two_validator(passports):
+    ValidPassports = 0
+    # define valid fields
+    field_list = ['byr:(19[2-9]\d|200[0-2])', 'iyr:(201\d|2020)', 
+                  'eyr:(202\d|2030)', 'hgt:1([5-8]\d|9[0-3])cm|hgt:(59|6\d|7[0-3])in',
+                  'hcl:#[a-z0-9]{6,}', 'ecl:(amb|blu|brn|gry|grn|hzl|oth)',
+                  'pid:\d{9,}']
+    fields = re.compile('|'.join(field_list))
+    for passport in passports:
+        if len(fields.findall(passport)) == 7:       
+            ValidPassports += 1
+        else:
+            pass
+    return ValidPassports
 
 def test_validator(test_input):
-    assert validator(test_input) == 2, 'Should be 2 valid passports'
+    assert part_one_validator(test_input) == 2, 'Should be 2 valid passports'
 
 if __name__ == '__main__':
     if run_test:
         with open('./inputs/day4_test.txt', 'r') as test_input:
-            test_validator(test_input.read())
+            test_validator(test_input.read().split('\n\n'))
             print('Test passed')
     else:
         with open('./inputs/day4.txt', 'r') as f:
-            my_password_list = f.read()
-            print(validator(my_password_list))
+            passports = f.read().split('\n\n')
+            print('The number of valid passports in part one is {}.'.format(part_one_validator(passports)))
+            print('The number of valid passports in part two is {}.'.format(part_two_validator(passports)))
