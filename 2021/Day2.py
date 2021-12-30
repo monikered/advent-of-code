6f648a649f1b98cd7f56b440850979fd235e31eb
monikered <monica.remmers@gmail.com>
2021-12-30 00:09:43 -0600

    day 2 pseudocode on main

---
+++ 2021/Day2.py
@@ -0,0 +1,10 @@
+x = 0
+y = 0
+
+# replace "forward" with "x +="; "up" with "y +="; "down" with "-="
+sub_key = {"forward": "x +=", "up": "y +=", "down": "y -="}
+
+# run each line of new input
+
+# # PART ONE 
+print("The final coordinates are a horizontal position of {} and a depth of {}. Their product is {}.".f(x, abs(y), x*abs(y)))
