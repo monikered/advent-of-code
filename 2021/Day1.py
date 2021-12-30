d81529673250eefa25230e7e3f31806794b53011
monikered <monica.remmers@gmail.com>
2021-12-29 20:06:30 -0600

    part one works!

--- 2021/Day1.py
+++ 2021/Day1.py
@@ -1,22 +1,22 @@
 # How many measurements are larger than the previous measurement?
 
+from numpy.core.numeric import NaN
 import pandas as pd
 import numpy as np
 
-with open('./2021/inputs/day1test.txt', 'r') as f:
-    depths = pd.DataFrame([i.strip() for i in f.readlines()], columns=['value'])
+with open('./2021/inputs/day1.txt', 'r') as f:
+    depths = pd.DataFrame([int(i) for i in f.readlines()], columns=['value'])
     depths['change'] = np.NaN
 
-# mark 'change' column as 1 if there's a row-on-row increase
-def is_increasing(df):
-    for i in range(1, len(df)):
-        if df.loc[i, 'value'] > df.loc[i-1, 'value']:
-            return 1
-        else:
-            return 0
-
-# PART ONE
-depths.apply(is_increasing, axis=1)
+for i in range(len(depths)):
+    if i == 0:
+        pass
+    elif depths.iloc[i, 0] > depths.iloc[i-1, 0]:
+        depths.iloc[i, 1] = 1
+    else:
+        depths.iloc[i, 1] = 0
+# # PART ONE
+# depths.apply(is_increasing, axis=1)
 print(depths)
-# answer should be 1462
-print(sum(depths['change'])+1)
+# # answer should be 1462
+print('The answer is '+ str(np.sum(depths['change'])))
