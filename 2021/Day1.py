ad601ab0bbc40fcb40cdd450f2780558d42b3bd4
monikered <monica.remmers@gmail.com>
2021-12-29 23:40:18 -0600

    messy solution to part two!

--- 2021/Day1.py
+++ 2021/Day1.py
@@ -4,7 +4,7 @@ from numpy.core.numeric import NaN
 import pandas as pd
 import numpy as np
 
-with open('./2021/inputs/day1test.txt', 'r') as f:
+with open('./2021/inputs/day1.txt', 'r') as f:
     depths = pd.DataFrame([int(i) for i in f.readlines()], columns=['value'])
     depths['change'] = np.NaN
 
@@ -19,25 +19,26 @@ for i in range(len(depths)):
         depths.iloc[i, 1] = 0
 
 # # answer should be 1462
-print('The answer is '+ str(np.sum(depths['change'])))
+print('The answer to part one is '+ str(np.sum(depths['change'])))
 
 # # PART TWO
 
+# this is so messy but hey it works
+
 triplets = pd.DataFrame([depths.iloc[int(i):int(i)+3, 0] for i in range(len(depths))])
-print(triplets)
-
 triplet_sums = pd.Series([triplets[i -2] + triplets[i - 1] + triplets[i] for i in range(2, len(triplets))])
-# for triplet in triplets:
-#     print(triplet)
+triplet_sums_as_df = pd.DataFrame([np.max(triplet_sums[i]) for i in range(len(triplet_sums))])
+triplet_sums_as_df['change'] = np.NaN
 
-# triple_sums = pd.DataFrame(depths['value'] for i in range(len(depths))], columns=['triple_sum'])
+print(triplet_sums_as_df)
 
-# print(triple_sums)
+for i in range(len(triplet_sums_as_df)):
+    if i == 0:
+        pass
+    elif triplet_sums_as_df.iloc[i, 0] > triplet_sums_as_df.iloc[i-1, 0]:
+        triplet_sums_as_df.iloc[i, 1] = 1
+    else:
+        triplet_sums_as_df.iloc[i, 1] = 0
 
-# for i in range(len(triple_sums)):
-#     if i == 0:
-#         pass
-#     elif triple_sums.iloc[i, 0] > triple_sums.iloc[i-1, 0]:
-#         triple_sums.iloc[i, 1] = 1
-#     else:
-#         triple_sums.iloc[i, 1] = 0
+# The answer is 1497
+print('The answer to part two is '+ str(np.sum(triplet_sums_as_df['change'])))
