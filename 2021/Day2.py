3103eec3ebe02bc90a3568c2c678e9ac613d3d9e
monikered <monica.remmers@gmail.com>
2021-12-30 22:07:16 -0600

    just need to figure out how to decode directions!

--- 2021/Day2.py
+++ 2021/Day2.py
@@ -1,10 +1,28 @@
-x = 0
-y = 0
+import pandas as pd
+
+with open('./2021/inputs/day2test.txt', 'r') as f:
+    directions = f.read().strip().split("\n")
+
+print(directions)
+
+# set origin
+x, y = 0, 0
 
 # replace "forward" with "x +="; "up" with "y +="; "down" with "-="
-sub_key = {"forward": "x +=", "up": "y +=", "down": "y -="}
+sub_key = {'forward' : 'x +=', 'up' : 'y+=', 'down' : 'y-='}
 
+# decoded_directions = []
+# for line in directions:
+#     decoded_directions.append(sub_key.get(line, line))
 # run each line of new input
+# directions_decoded = "\n".join(sub_key.get(ele, ele) for ele in directions)
+for code, math in sub_key.items():
+    directions = [direction.replace(code, math) for direction in directions]
+
+for line in directions:
+    pd.eval(line)
+
+# execute the decoded directions
 
 # # PART ONE 
-print("The final coordinates are a horizontal position of {} and a depth of {}. Their product is {}.".f(x, abs(y), x*abs(y)))
+print(f'The final coordinates are a horizontal position of {x} and a depth of {abs(y)}. Their product is {x*abs(y)}.')
