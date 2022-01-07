35ea43d14e256c479b223d9b31dad887a5f20bab
monikered <monica.remmers@gmail.com>
2022-01-06 22:06:33 -0600

    Day 2 solved and pretty

--- 2021/Day2.py
+++ 2021/Day2.py
@@ -1,18 +1,40 @@
 import pandas as pd
 
-with open('./2021/inputs/day2test.txt', 'r') as f:
+with open('./2021/inputs/day2.txt', 'r') as f:
     directions = f.read().strip().split("\n")
 
-x, y = 0, 0
 
-for line in directions:
-    dir, dist = line.split()
-    if dir == 'down':
-        y -= int(dist)
-    elif dir == 'up':
-        y += int(dist)
-    else:
-        x += int(dist)
+def part_one_solution(dirs=directions):
 
-# # # PART ONE 
-print(f'The final coordinates are a horizontal position of {x} and a depth of {abs(y)}. Their product is {x*abs(y)}.')
+    x = y = 0
+
+    for line in directions:
+        dir, dist = line.split()
+        if dir == 'down':
+            y -= int(dist)
+        elif dir == 'up':
+            y += int(dist)
+        else:
+            x += int(dist)
+
+    print(f'PART ONE: The final coordinates are a horizontal position of {x} and a depth of {abs(y)}. Their product is {x*abs(y)}.')
+
+def part_two_solution(dirs=directions):
+
+    x = y = z = 0
+    
+    for line in directions:
+        dir, dist = line.split()
+        if dir == 'down':
+            z += int(dist)
+        elif dir == 'up':
+            z -= int(dist)
+        else:
+            x += int(dist)
+            y = y + z*int(dist)
+
+    print(f'PART TWO: The final coordinates are a horizontal position of {x} and a depth of {abs(y)}. Their product is {x*abs(y)}.')
+
+if __name__ == '__main__':
+    part_one_solution()
+    part_two_solution()
